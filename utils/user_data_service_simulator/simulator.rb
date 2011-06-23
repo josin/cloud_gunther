15063672c89c93925e4d4fdd49472e11952fe6ec
Josef Sin <josef.sin@gmail.com>
2011-06-23 22:48:27 +0200

    Added simulator for instance user data...

---
+++ utils/user_data_service_simulator/simulator.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+%w{rubygems sinatra yaml}.each { |lib| require lib }
+
+# set :port, 80
+
+# User data => http://169.254.169.254/latest/user-data/
+get '/latest/user-data' do
+  {
+    :amqp_config => {
+      :host => "localhost", 
+      :port => 5672, 
+      :user => "josin", 
+      :pass => "josin", 
+      :vhost => "/", 
+      :timeout => 3600, 
+      :logging => false, 
+      :ssl => false },
+    :input_queue => "inputs",
+    :output_queue => "outputs",
+  }.to_yaml
+end
+
+
+# Instance id => http://169.254.169.254/latest/meta-data/instance-id
+get '/latest/meta-data/instance-id' do
+  "i-123456"
+end
+
+
