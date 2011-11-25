f0fccdf6d5d98bd6d6cdd3909eb222f103933667
Josef Sin <josef.sin@gmail.com>
2011-11-25 11:57:36 +0100

    Change password from admin correct redirect.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,13 +78,16 @@ class UsersController < ApplicationController
   # PUT /users/1.xml
   def update
     @user = User.find(params[:id])
-
+    
     respond_to do |format|
       if @user.update_attributes(params[:user])
         format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
         format.xml  { head :ok }
       else
-        format.html { render :action => "edit" }
+        format.html do
+          action_to_render = ("Change password" == params[:commit].presence) ? "change_password" : "edit"
+          render :action => action_to_render
+        end
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
     end
