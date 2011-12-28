a2e814c9aa6dce1a187ce01a6f932c81d2a4f9fc
Josef Sin <josef.sin@gmail.com>
2011-12-28 20:09:50 +0100

    Task runner spec.

--- spec/workers/task_runner_spec.rb
+++ spec/workers/task_runner_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe TaskRunner do
-  pending
+  let(:task) { mock_model(Task).as_null_object }
+  
+  describe "self.perform method" do
+    it "should run the task" do
+      Task.should_receive(:find).with(task.id).and_return(task)
+      task.should_receive(:run)
+      TaskRunner.perform(task.id)
+    end
+  end
 end
